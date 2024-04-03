75cff4a617dc129e741b9b3f412f9d1b0a3c2247
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-04-03 08:27:48 +0530

    default files added

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-import { Command } from "@codemonument/cliffy/command";
+import { Command } from "@codemonument/cliffy/command"
 import { Confirm } from "@codemonument/cliffy/prompt"
-import * as path from "@std/path";
-import {copy} from "@std/fs";
+import * as path from "@std/path"
 
 await new Command()
   .description(
@@ -12,15 +11,20 @@ await new Command()
   .version("1.0.0")
   .name("dph")
   .action(async function() {
-    console.log(import.meta.dirname);
-    if (!await exists(path.normalize(Deno.cwd() + '/deno.json'))) {
-      console.log("Project does not exists");
+    console.log(import.meta.dirname)
+    if (!await exists(path.normalize(Deno.cwd() + "/deno.json"))) {
+      console.log("Project does not exists")
     }
-    const result = await Confirm.prompt("Do you want to sync default files");
+    const result = await Confirm.prompt("Do you want to sync default files")
 
     if (result) {
-      console.log("Replacing default files");
-      await copy(path.normalize(import.meta.dirname+'/project-commons/.editorconfig'), Deno.cwd(), {overwrite: true});
+      console.log("Replacing default files")
+      let fileContent = await Deno.readTextFile(path.normalize(import.meta.dirname + "/project-commons/.editorconfig"))
+      await Deno.writeTextFile("./.editorconfig", fileContent, { create: true })
+      fileContent = await Deno.readTextFile(path.normalize(import.meta.dirname + "/project-commons/.gitignore"))
+      await Deno.writeTextFile("./.gitignore", fileContent, { create: true })
+      fileContent = await Deno.readTextFile(path.normalize(import.meta.dirname + "/project-commons/.prettierrc"))
+      await Deno.writeTextFile("./.prettierrc", fileContent, { create: true })
     }
   })
   .parse(Deno.args)
@@ -31,16 +35,16 @@ await new Command()
  */
 export async function exists(filePath: string): Promise<boolean> {
   try {
-    await Deno.stat(filePath);
+    await Deno.stat(filePath)
     // successful, file or directory must exist
-    return true;
+    return true
   } catch (error) {
     if (error instanceof Deno.errors.NotFound) {
       // file or directory does not exist
-      return false;
+      return false
     } else {
       // unexpected error, maybe permissions, pass it along
-      throw error;
+      throw error
     }
   }
 }
